2734f060b84ac0a1912bfd0fa331b9cee9fd6119
Yang <38071744+crea9813@users.noreply.github.com>
2023-05-26 09:58:59 +0900

    Initial Commit

---
+++ EarBuds/EarBudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarBudsApp.swift
+//  EarBuds
+//
+//  Created by 매기 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct EarBudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
